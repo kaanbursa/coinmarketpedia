43f5c42c92a0940011d31ab89196a734d0cf48cd
kaan bursa <kaanbursa9@gmail.com>
2018-01-31 09:40:36 +0100

    browser and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => (
   <footer className="footer">
     <div className="text-center">
-      © 2017 <a href="" target="_blank">CoinMarketPedia</a>
+      © 2017 <Link to="/" className="footer-text" target="_blank">CoinMarketPedia</Link>
+      <a className="footer-text" href="mailto:kaan@coinmarketpedia.com?Subject=Hello" target="_blank">Contact Us!</a>
+      <Link className="footer-text" to='/register'>About </Link> 
     </div>
   </footer>
 );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { hashHistory, Router } from 'react-router';
+import { hashHistory, browserHistory, Router } from 'react-router';
 import routes from './routes.js';
 import Auth from './modules/auth.js';
 
@@ -12,5 +12,5 @@ import Auth from './modules/auth.js';
 
 ReactDom.render((
   <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <Router history={hashHistory} routes={routes} />
+    <Router history={browserHistory} routes={routes} />
   </MuiThemeProvider>), document.getElementById('app'));
